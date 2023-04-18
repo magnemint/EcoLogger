bc4dc4af10292f963ed637779c8e8f4f5b873f88
Magnemint <129741368+magnemint@users.noreply.github.com>
2023-04-18 20:13:46 +0800

    Create LoggerCasualEnum.java

---
+++ 0.2.0-beta/code/enums/LoggerCasualEnum.java
@@ -0,0 +1,14 @@
+package your.directory.here.enums;
+
+public enum LoggerCasualEnum {
+	
+	/*
+	 * @author: magnemint
+	*/
+	
+  	//Add your own custom fields here :)
+	ClientInfo,
+	LoginStatus,
+	Misc;
+
+}
